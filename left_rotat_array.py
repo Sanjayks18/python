b676842dacd132d51927ac9e6c024ba39903d55f
Sanjayks18 <sanjusanjayks012345@gmail.com>
2025-08-30 19:24:56 +0530

    Add files via upload
    left rotation of an array of d elements

---
+++ left_rotat_array.py
@@ -0,0 +1,21 @@
+#rotat left a array of d elements
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    arr.append(int(input(f"enter {i+1} th element: ")))
+
+d=int(input("enter D(number of elements to be rotat):"))
+
+print(arr)
+n=len(arr)
+
+temp=arr[:d]
+#shifting
+for i in range(d,n):
+    arr[i-d]=arr[i]
+#addback at end
+for i in range(n-d,n):
+    arr[i]=temp[i-(n-d)]
+
+print("compleated",arr)
+    
